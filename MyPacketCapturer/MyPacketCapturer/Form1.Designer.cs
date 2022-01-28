c8faababdb4a49b9eac8c8009ef2985c08594d06
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2022-01-28 12:01:51 -0500

    Packet capture software (Lab 3)

---
+++ MyPacketCapturer/MyPacketCapturer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MyPacketCapturer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.txtCaptureData = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartStop.Location = new System.Drawing.Point(366, 12);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(104, 47);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(115, 80);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(623, 24);
+            this.cmbDevices.TabIndex = 1;
+            this.cmbDevices.SelectedIndexChanged += new System.EventHandler(this.cmbDevices_SelectedIndexChanged);
+            // 
+            // txtCaptureData
+            // 
+            this.txtCaptureData.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCaptureData.Location = new System.Drawing.Point(49, 123);
+            this.txtCaptureData.Multiline = true;
+            this.txtCaptureData.Name = "txtCaptureData";
+            this.txtCaptureData.Size = new System.Drawing.Size(746, 630);
+            this.txtCaptureData.TabIndex = 2;
+            this.txtCaptureData.TextChanged += new System.EventHandler(this.txtCaptureData_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 775);
+            this.Controls.Add(this.txtCaptureData);
+            this.Controls.Add(this.cmbDevices);
+            this.Controls.Add(this.btnStartStop);
+            this.Name = "Form1";
+            this.Text = "Packet Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.TextBox txtCaptureData;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MyPacketCapturer/MyPacketCapturer/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PacketDotNet;
+using SharpPcap;
+
+namespace MyPacketCapturer
+{
+    public partial class Form1 : Form
+    {
+        CaptureDeviceList devices; //List of devices for this computer
+        public static ICaptureDevice device; //Device to be used
+        public static string stringPackets = ""; //Data that is captured
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Get the list of devices
+            devices = CaptureDeviceList.Instance;
+
+
+            //Make sure that there is at least one device
+            if(devices.Count < 1)
+            {
+                MessageBox.Show("No Capture Devices Found");
+                Application.Exit();
+            }
+
+            //Add devices to combo box
+            foreach(ICaptureDevice dev in devices)
+            {
+                cmbDevices.Items.Add(dev.Description);
+            }
+
+            //Get the second device and displpay in combo box
+            device = devices[3];
+            cmbDevices.Text = device.Description;
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capturing
+            int readTimeoutMiliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMiliseconds);
+
+        }
+
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs packet)
+        {
+            //Array to store our data
+            byte[] data = packet.Packet.Data;
+
+            //Keep track of number of bytes displayed per line
+            int byteCounter = 0;
+
+            //Process each byte in our capture packet
+            foreach(byte b in data)
+            {
+                //Add byte to our string in hex
+                stringPackets += b.ToString("x2") + " ";
+                byteCounter++;
+
+                if(byteCounter == 16)
+                {
+                    byteCounter = 0;
+                    stringPackets += Environment.NewLine;
+                }
+            }
+            stringPackets += Environment.NewLine;
+            stringPackets += Environment.NewLine;
+        }
+
+        private void txtCaptureData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(btnStartStop.Text == "Start")
+                {
+                    device.StartCapture();
+                    timer1.Enabled = true;
+                    btnStartStop.Text = "Stop";
+                } 
+                else
+                {
+                    device.StopCapture();
+                    timer1.Enabled = false;
+                    btnStartStop.Text = "Start";
+                }
+            }
+            catch(Exception exc)
+            {
+
+            }
+        }
+
+        //Dump packet data from stringPackets to text box
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtCaptureData.AppendText(stringPackets);
+            stringPackets = "";
+        }
+
+        private void cmbDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            device = devices[cmbDevices.SelectedIndex];
+            cmbDevices.Text = device.Description;
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capturing
+            int readTimeoutMiliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMiliseconds);
+        }
+    }
+}
